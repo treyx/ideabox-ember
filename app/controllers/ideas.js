d02d38a81f59469d7ae58d761a07c4c6f1784b76
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-03 11:27:09 -0600

    basic create action in ideas controller

--- app/controllers/ideas.js
+++ app/controllers/ideas.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  title: null,
+  body: null,
+
+  actions: {
+    addNewIdea: function () {
+      let idea = this.getProperties('title', 'body');
+      this.store.createRecord('idea', idea).save();
+    }
+  }
 });
