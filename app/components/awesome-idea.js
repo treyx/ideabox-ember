8fd28dd4fbc06fb4a0000d9f572536a44f4d9efe
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-03 11:50:25 -0600

    added edit function

--- app/components/awesome-idea.js
+++ app/components/awesome-idea.js
@@ -2,8 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  editing: false,
+
   actions: {
-    deleteIdea: function () {
+
+    edit: function () {
+      let editing = this.get('editing');
+      if (editing) {
+        this.get('idea').save().then(() => {
+          this.set('editing', false);
+        })
+      } else {
+        this.set('editing', true);
+      }
+    },
+    delete: function () {
       this.get('idea').destroyRecord();
     }
   }
