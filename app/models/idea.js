5a237191dab96573ff14a68f0a5ae9b04a3ba972
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-03 10:48:49 -0600

    added fixtures to idea model

--- app/models/idea.js
+++ app/models/idea.js
@@ -1,7 +1,15 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+let Idea = DS.Model.extend({
   title: DS.attr('string'),
   body: DS.attr('string')
-
 });
+
+Idea.reopenClass({
+  FIXTURES: [
+    { id: 1, title: "First Idea", body: "First Lorem Ipsum" },
+    { id: 2, title: "Second Idea", body: "Second Lorem Ipsum" }
+  ]
+});
+
+export default Idea;
